0ee1b1b64ec0882eea48a0e820a4794cf0af198c
oeliteo <woolseytrindon@gmail.com>
2025-09-21 21:04:00 -0600

    Update main.py with new functions

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+def main():
+    count_words(get_book_text("./books/frankenstein.txt"))
+    return
+
+
+def get_book_text(relative_path):
+    with open(relative_path) as f:
+        return f.read()
+
+
+def count_words(text):
+    count = 0
+    for word in text.split():
+        count += 1
+    print(f"{count} words found in the document")
+
+
+main()
