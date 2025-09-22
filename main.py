871f56658f2cd6b7b263b76a0a8d23538399ff8c
oeliteo <woolseytrindon@gmail.com>
2025-09-21 22:52:47 -0600

    Updated stats.py, main.py helper functions + sys module for sys.argv command arguments when running bookbot.

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-from stats import count_words
+import sys
+
+from stats import (
+    count_characters,
+    count_words,
+    seperate_character_dictionaries,
+    sort_helper,
+)
 
 
 def main():
-    count_words(get_book_text("./books/frankenstein.txt"))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
+    word_count = count_words(book_text)
+    char_count = count_characters(book_text)
+    char_dict_list = seperate_character_dictionaries(char_count)
+    char_dict_list.sort(key=sort_helper, reverse=True)
+
+    head = f"""============ BOOKBOT ============
+            Analyzing book found at {book_path}...
+            ----------- Word Count ----------
+            Found {word_count} total words
+            --------- Character Count -------"""
+
+    tail = """============= END ==============="""
+    head = head.split("\n")
+    for line in head:
+        print(line.lstrip())
+    print_char_dict_list_lines(char_dict_list)
+    print(tail)
+
     return
+
+
+def print_char_dict_list_lines(dict_list):
+    for item in dict_list:
+        print(f"{item["char"]}: {item["num"]}")
 
 
 def get_book_text(relative_path):
--- stats.py
+++ stats.py
@@ -2,4 +2,31 @@ def count_words(text):
     count = 0
     for word in text.split():
         count += 1
-    print(f"{count} words found in the document")
+    return count
+
+
+def seperate_character_dictionaries(character_counts):
+    dictionary_list = []
+    for key in character_counts:
+        if key.isalpha():
+            dictionary_list.append({"char": key, "num": character_counts[key]})
+        else:
+            pass
+    return dictionary_list
+
+
+def count_characters(text):
+    text = text.lower()
+    character_dictionary = {}
+
+    for char in text:
+        if char not in character_dictionary:
+            character_dictionary[char] = 1
+        else:
+            character_dictionary[char] += 1
+
+    return character_dictionary
+
+
+def sort_helper(char_dict):
+    return char_dict["num"]
